6d189201f670d1f05952978fc69a400b263c886e
pujikun <107119422+puji8493@users.noreply.github.com>
2022-06-11 10:09:17 +0900

    Add files via upload

---
+++ kiso_file_sousa.py
@@ -0,0 +1,19 @@
+#pyファイルを保存しているフォルダから、"kiso"の文字列のテキストファイル名を抜き出す
+import os
+
+#OSモジュールで、カレントディレクトリを表す
+print(os.getcwd())
+
+#カレントディレクトリを変更する
+os.chdir("C:/Users/arupa/Documents/python/TestDir")
+print(os.getcwd())
+#元に戻す
+os.chdir("C:/Users/arupa/Documents/python/py_File")
+print(os.getcwd())
+
+#カレントディレクトリ内のファイルを表示する
+print(os.listdir())
+for c in os.listdir():
+    print(c)
+
+
