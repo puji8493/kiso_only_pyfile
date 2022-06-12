02a2a64a0b36b863a812f3116403f2626c0ef2a4
pujikun <107119422+puji8493@users.noreply.github.com>
2022-06-12 16:33:12 +0900

    Add files via upload

--- kiso05.py
+++ kiso05.py
@@ -1,4 +1,5 @@
 #残業時間が100時間を超えているを要注意リストに格納して、書き出す
+#計算式をCellオブジェクトから取得したい時は、引数　data_only=Trueを追加する
 
 import openpyxl
 
@@ -9,20 +10,24 @@ wb = openpyxl.load_workbook(ph,data_only=True)
 shFm = wb["元データ"]
 shTo = wb["要注意リスト"]
 
-attention_lst = []
-#合計が１００時間以上の場合は、要注意リストに格納する
+#残業リストを作る
+zangyo_lst = []
 for row in shFm.iter_rows(min_row=4,max_row=13,max_col=9):
-    goukei = row[8].value
-    if goukei > 100:
-        attention_lst.append([row[0].value,row[1].value,row[8].value])
-        print(attention_lst)
+    #行の空リストをつくる
+    ls = []
+    for r in row:
+        ls.append(r.value)
+    zangyo_lst.append(ls)
 
-#要注意リストに格納したリストを転記する 開始行はインデックス9
-r_idx = 9　
-for lst in attention_lst:
-    shTo.cell(row=r_idx,column=3).value = lst[0]
-    shTo.cell(row=r_idx,column=4).value = lst[1]
-    shTo.cell(row=r_idx,column=5).value = lst[2]
-    r_idx = r_idx + 1
+#合計列が１００時間以上の場合は、要注意リストに格納する
+a_list = [t for t in zangyo_lst if t[8] >= 100]
+print(a_list)
+
+#sheets"要注意リスト"に、100時間残業敬の人を書き出す
+#リストの合計時間はI列（インデックス番号8)
+for c,v in enumerate(a_list):
+    shTo.cell(row=c+9,column=3).value = v[0]
+    shTo.cell(row=c+9,column=4).value = v[1]
+    shTo.cell(row=c+9,column=5).value = v[8]
 
 wb.save(mod_ph)
