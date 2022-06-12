bef3265f9f5a46ab5aa118b6b209715b51714acc
pujikun <107119422+puji8493@users.noreply.github.com>
2022-06-12 19:44:13 +0900

    Add files via upload

--- kiso11_2.py
+++ kiso11_2.py
@@ -1,10 +1,9 @@
-#当月の支出リストを格納
-#総元帳に転記する　最終行に合計額を転記する
+#当月の支出リストに格納した後、総元帳に転記する　最終行に合計額を転記する
 #当月（4月）の支出項目を辞書で登録　キーが重複した時は、値を加算する　連想配列で値の累計を出すイメージ
 
 import openpyxl
 ph = "../learning/ks011.xlsx"
-mod_ph = "../learning/ks011_mod_dic.xlsx"
+mod_ph = "../learning/ks011_mod_dic2.xlsx"
 wb = openpyxl.load_workbook(ph)
 sh = wb["Sheet1"]
 
@@ -25,22 +24,33 @@ pay_thismonth = 0
 pay_total = 0
 
 #Ｂ列の項目と一致したら、4月支出金額の値をＤ列今月支払額に転記する
-for r in sh["B4:E9"]:
-    koumoku = r[0].value
+#A列からC列　総覧表をリストに格納
+souran_list = []
+for row in sh.iter_rows(min_row=4,max_row=9,max_col=3):
+    #行をリストに格納
+    ls = []
+    for r in row:
+        ls.append(r.value)
+    #総覧リストに格納
+    souran_list.append(ls)
+
+for c,i in enumerate(souran_list):
+    koumoku = i[1]
     #項目が辞書に存在しない場合は、総計に転記し、総支払額を追加してbreak文でループを抜ける
     #項目じ辞書に存在する場合は、今月支出額に転記し、今月累計額を加算してbreak文でループを抜ける
     for k,v in lst_Aplil.items():
         if koumoku not in lst_Aplil:
-            r[3].value = r[1].value
-            pay_total = pay_total + r[3].value
+            sh.cell(row=c+4,column=5).value = i[2]
+            pay_total = pay_total + sh.cell(row=c+4,column=5).value
             break
         elif k == koumoku:
-            r[2].value = v
-            r[3].value = r[1].value + r[2].value
-            pay_thismonth = pay_thismonth + r[2].value
-            pay_total = pay_total + r[3].value
+            sh.cell(row=c+4,column=4).value = v
+            sh.cell(row=c+4,column=5).value = v + sh.cell(row=c+4,column=3).value
+            pay_thismonth = pay_thismonth + sh.cell(row=c+4,column=4).value
+            pay_total = pay_total + sh.cell(row=c+4,column=5).value
             break
 
 sh["D10"].value = pay_thismonth
 sh["E10"].value = pay_total
 wb.save(mod_ph)
+
