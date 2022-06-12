9aeb710cf477165eb156297915cce9d13d504a34
pujikun <107119422+puji8493@users.noreply.github.com>
2022-06-12 15:50:07 +0900

    Add files via upload

--- kiso04.py
+++ kiso04.py
@@ -1,32 +1,39 @@
 ##100時間残業時間超過の人を要注意リストに格納して、書き出す
 import openpyxl
 ph = "../learning/ks004.xlsx"
-mod_ph = "../learning/ks004_mod.xlsx"
+mod_ph = "../learning/ks004_mod2.xlsx"
 wb = openpyxl.load_workbook(ph)
 sh = wb["Sheet1"]
 
-#8行目から１７行目までのＩＤ、名前、合計をリストに格納
-lst = []
 #データが入力されているセルの最大行は「max_row」最大列は「max_column」で取得
-max_row = sh.max_row
-print(max_row)
+max_gyou = sh.max_row
+print(max_gyou)
 
-# 範囲指定するセル番地の場合は文字列で変換
-for row in sh["A8:C"+str(max_row)]:
-    lst.append(row)#リストに格納
+#全員の残業リスト
+zangyo_list = []
+for row in sh.iter_rows(min_row=8,max_row=max_gyou,max_col=3):
+    #行の空リスト
+    lst = []
+    for r in row:
+        lst.append(r.value)
+    zangyo_list.append(lst)
+    #print(lst)
+    #実行結果
+    #[1, '五日市 加奈子', 139]
+    #[2, '呉 早希', 65]
+    #[3, '高岡 範夫', 0]
 
-attention_lst =[]
-#リストを出力し、1行ごとに合計が100時間以上であれば、要注意リストに再格納
-for r in lst:
-    goukei = r[2].value
-    if goukei > 100:
-        attention_lst.append(r)
-#要注意リストを書き出す
-r_idx = 5
-for st in attention_lst:
-    sh.cell(row=r_idx,column=6).value = st[0].value
-    sh.cell(row=r_idx,column=7).value = st[1].value
-    sh.cell(row=r_idx,column=8).value = st[2].value
-    r_idx = r_idx + 1
+#全員の残業リストから、100時間以上の人を要注意リストに格納する
+#「リスト内包表記」で、ループ処理を１行に繋げて書くことができます。
+#100時間以上の残業対象者をリスト内包表記でリストに格納する
+# [仮変数を用いた式 for 仮変数 in イテラブルオブジェクト（反復可能なオブジェクト）if 条件式]
+a_list = [t for t in zangyo_list if t[2]>=100]
+print(a_list)
+
+for c,v in enumerate(a_list):
+    #要注意リストの要素を書き出す　v[0]=ID　v[1]=名前 v[2]=合計
+    sh.cell(row=5+c,column=6).value = v[0]
+    sh.cell(row=5+c,column=7).value = v[1]
+    sh.cell(row=5+c,column=8).value = v[2]
+
 wb.save(mod_ph)
-
