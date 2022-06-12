1e59e4d2726f5c4ab0b887384b6fedc76f2c2c9e
pujikun <107119422+puji8493@users.noreply.github.com>
2022-06-12 22:25:31 +0900

    Add files via upload

--- kiso_txt_output.py
+++ kiso_txt_output.py
@@ -1,4 +1,4 @@
-#pyファイルの内、kisoの文字列のファイル名だけを格納したリストを作り、txtファイルに保存する
+#pyファイルの内、kisoの文字列のファイル名だけを格納したリストを作り、csvファイルに保存する
 #ファイル全般操作はpathlib
 #改行するためには、リストの文字列に ".\n"　を追加するのがポイント
 
@@ -6,15 +6,12 @@ import os
 from pathlib import Path
 
 #書き出して保存するファイルパス
-output_path = Path("../learning/kiso_output.txt")
+output_path = Path("../learning/kiso_output_3.txt")
 
 #カレントディレクトリのうち、"kiso"の文字列のファイル名だけ抜き出して、リストに格納
-ls = []
-for c in os.listdir():
-    if "kiso" in c:
-        gyo = c + ".\n"
-        ls.append(gyo)
+#1行毎に t + "\n"の改行マークを入れる
 
+ls = [t + "\n" for t in os.listdir() if "kiso" in t]
 for c in ls:
     print(c)
 
@@ -22,3 +19,13 @@ for c in ls:
 with output_path.open(mode="w") as f:
     for i in ls:
         f.write(i)
+
+
+'''
+ls = []
+for c in os.listdir():
+    if "kiso" in c:
+        gyo = c + ".\n"
+        ls.append(gyo)
+
+'''
